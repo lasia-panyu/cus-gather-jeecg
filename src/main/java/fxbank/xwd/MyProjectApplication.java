7678a17e8e12d658528634ba1add340b3254dc1b
panyu <panyu@fuxinbank.com>
2021-12-02 11:03:42 +0800

    信息采集平台管理端(jeecg框架)

---
+++ src/main/java/fxbank/xwd/MyProjectApplication.java
@@ -0,0 +1,16 @@
+package fxbank.xwd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhouhao
+ * @since 1.0
+ */
+@SpringBootApplication
+public class MyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyProjectApplication.class,args);
+    }
+}
